b01f93c80304283bc12deedae38ccec3ec86635a
Dangocan <danilogc1999@gmail.com>
2023-10-08 06:53:33 -0300

    wip: validates the containers inter comunication

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,36 @@
 import express from "express";
-const app = express();
-const port = 3001;
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+dotenv.config({ path: __dirname + "/../.env" });
+
+const app = express();
+// MongoClient.connect(
+//   `mongodb://${process.env.DB_HOSTNAME}:${process.env.DB_PORT}/${process.env.DB_DATABASE}}`
+// )
+//   .then((result) => {
+//     console.log("MognoDB connection success");
+//     console.log(result);
+//   })
+//   .catch((err) => {
+//     console.log("MognoDB connection error");
+//     console.log(err.message);
+//   });
+
+app.listen(3001, () => {
+  console.log(`Example app listening on port ${process.env.BACKEND_PORT}`);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get("/", async (req, res) => {
+  await MongoClient.connect(
+    `mongodb://${process.env.DB_HOSTNAME}:${process.env.DB_PORT}/${process.env.DB_DATABASE}}`
+  )
+    .then((result) => {
+      res.send("MognoDB connection success");
+      console.log(result);
+    })
+    .catch((err) => {
+      res.send("MognoDB connection error");
+      console.log(err.message);
+    });
 });
