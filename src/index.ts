5ec58e97f84055a9b32845455fe3cba6db27063c
Dangocan <danilogc1999@gmail.com>
2023-10-09 03:50:06 -0300

    feat: add new middlewares and new server start msg

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectToDatabase } from "./utils/database.utils";
+import { AuthRouter } from "./auth/auth.controller";
 
 dotenv.config({ path: __dirname + "/../.env" });
 
 const app = express();
+app.use(express.json());
+app.use(AuthRouter);
 
-app.listen(process.env.BACKEND_PORT, () => {
-  console.log(`Example app listening on port ${process.env.BACKEND_PORT}`);
-});
-
-app.get("/", async (req, res) => {
-  const { connectionSuccess } = await connectToDatabase();
-  res.send("Hello World! " + connectionSuccess);
+connectToDatabase().then(({ connectionSuccess }) => {
+  app.listen(process.env.BACKEND_PORT, () => {
+    console.log(
+      `Application listening on port ${process.env.BACKEND_PORT} ${
+        connectionSuccess ? "and successfully connected to database" : ""
+      }`
+    );
+  });
 });
