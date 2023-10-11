b9b68110f395b1daa353907c1dacb0199dc871a1
Dangocan <danilogc1999@gmail.com>
2023-10-11 02:14:41 -0300

    feat: board crud routes

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -7,6 +7,26 @@ const BoardRouter = Router();
 
 BoardRouter.use(checkJwtToken);
 
+BoardRouter.get(`${routePrefix}`, async (req, res) => {
+  const { currentUserId } = req.body;
+  try {
+    const allBoardsParticipating = await BoardService.getAll(currentUserId);
+    res.status(200).json(allBoardsParticipating);
+  } catch (error) {
+    res.status(500).json({ message: "Board search failed" });
+  }
+});
+
+BoardRouter.get(`${routePrefix}/:id`, async (req, res) => {
+  const { id } = req.params;
+  try {
+    const boardDetails = await BoardService.getBoardDetails(id);
+    res.status(200).json(boardDetails);
+  } catch (error) {
+    res.status(500).json({ message: "Board search failed" });
+  }
+});
+
 BoardRouter.post(`${routePrefix}`, async (req, res) => {
   const { title, usersAllowed, createdBy } = req.body;
   try {
@@ -18,6 +38,32 @@ BoardRouter.post(`${routePrefix}`, async (req, res) => {
     res.status(201).json(newBoard);
   } catch (error) {
     res.status(500).json({ message: "Board creation failed" });
+  }
+});
+
+BoardRouter.post(`${routePrefix}/:id`, async (req, res) => {
+  const { title, usersAllowed, createdBy } = req.body;
+  const { id } = req.params;
+  try {
+    const updatedBoard = await BoardService.updateBoard(
+      id,
+      title,
+      usersAllowed,
+      createdBy
+    );
+    res.status(201).json(updatedBoard);
+  } catch (error) {
+    res.status(500).json({ message: "Board update failed" });
+  }
+});
+
+BoardRouter.delete(`${routePrefix}/:id`, async (req, res) => {
+  const { id } = req.params;
+  try {
+    await BoardService.deleteBoard(id);
+    res.status(200).json({ message: "Board deleted" });
+  } catch (error) {
+    res.status(500).json({ message: "Board deletion failed" });
   }
 });
 
--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -9,6 +9,42 @@ const BoardService = {
     });
     return newBoard;
   },
+
+  async getAll(userId: string) {
+    const boardsParticipating = await BoardModel.find({
+      $or: [{ createdBy: userId }, { usersAllowed: userId }],
+    });
+
+    return boardsParticipating;
+  },
+
+  async getBoardDetails(id: string) {
+    const boardDeatil = await BoardModel.findById(id);
+
+    return boardDeatil;
+  },
+
+  async updateBoard(
+    id: string,
+    title: string,
+    usersAllowed: string[],
+    createdBy: string
+  ) {
+    const updatedBoard = await BoardModel.findByIdAndUpdate(
+      id,
+      {
+        title,
+        usersAllowed,
+        createdBy,
+      },
+      { new: true }
+    );
+    return updatedBoard;
+  },
+
+  async deleteBoard(id: string) {
+    return await BoardModel.findOneAndDelete({ _id: id });
+  },
 };
 
 export { BoardService };
